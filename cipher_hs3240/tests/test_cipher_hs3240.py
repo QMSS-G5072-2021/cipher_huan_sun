b8731788ceccdd57156e4b1d963133beeffdfffe
HuanSunGo <90728672+HuanSunGo@users.noreply.github.com>
2021-11-11 11:14:13 -0500

    add tests

--- cipher_hs3240/tests/test_cipher_hs3240.py
+++ cipher_hs3240/tests/test_cipher_hs3240.py
@@ -1,1 +1,10 @@
 from cipher_hs3240 import cipher_hs3240
+
+import pytest
+
+def test_cipher():
+    text = 'd'
+    expected = 'c'
+    actual = cipher_hs3240.cipher(text, -1)
+    assert actual == expected, "Cipher didn't worked."
+
